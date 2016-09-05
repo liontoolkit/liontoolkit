f48d983256428ff8b324ddd852ef2b85f5026f07
Andrea Mariello <andrea.mariello@unitn.it>
2016-09-05 19:29:10 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,14 @@
+# The LIONtoolkit project - http://liontoolkit.org/
+#
+# Authors:
+# Andrea Mariello <andrea.mariello@unitn.it>
+# Marco Zugliani <marco.zugliani@studenti.unitn.it>
+# Mauro Brunato <mauro.brunato@unitn.it>
+# Roberto Battiti <roberto.battiti@unitn.it>
+#
+# License:
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+"""test_app.py"""
